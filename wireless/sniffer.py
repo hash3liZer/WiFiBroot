ac3f079ff22adb883cce6c56cf80c179b509444f
hash3liZer <sheikhhshameerkashif@protonmail.com>
2019-09-24 06:57:22 +0500

    Changed Access Points

--- wireless/sniffer.py
+++ wireless/sniffer.py
@@ -1,20 +1,23 @@
 import os
 import re
 import sys
+import time
+import random
 import threading
+import subprocess
 from scapy.sendrecv import sniff
 from scapy.layers.dot11 import RadioTap
 from scapy.layers.dot11 import Dot11
 from scapy.layers.dot11 import Dot11Beacon
 from scapy.layers.dot11 import Dot11Elt
-from scapy.layers.dot11 import Dot11RSN
+from scapy.layers.dot11 import Dot11EltRSN
 from scapy.layers.dot11 import Dot11FCS
+from scapy.layers.dot11 import Dot11EltMicrosoftWPA
 from scapy.layers.eap   import EAPOL
 
 class SNIFFER:
 
-	__ACCESSPOINTS = []
-	__STATIONS     = {}
+	__ACCESSPOINTS = {}
 
 	def __init__(self, interface, channels, essids, aps, stations, filters):
 		self.interface = interface
@@ -81,16 +84,81 @@ class SNIFFER:
 	def extract_encryption(self, pkt):
 		retval = ''
 
-		if pkt.haslayer(Dot11RSN):
+		if pkt.haslayer(Dot11EltRSN):
 			retval = 'WPA2'
+
+		if pkt.haslayer(Dot11EltMicrosoftWPA):
+			retval += '/WPA' if retval else 'WPA'
+
+		if not retval:
+			try:
+				cap = str(pkt.getlayer(Dot11FCS).cap)
+			except:
+				cap = str(pkt.getlayer(Dot11).cap)
+
+			if "privacy" in cap.split("+"):
+				retval = 'WEP'
+			else:
+				retval = 'OPN'
+
+		return retval
+	def extract_cipher(self, pkt):
+		retval = ''
+		aciphers = {
+			1: 'WEP',
+			2: 'TKIP',
+			4: 'CCMP',
+			5: 'WEP'
+		}
+
+		if pkt.haslayer(Dot11EltRSN):
+			rsnlayer = pkt.getlayer(Dot11EltRSN)
+			ciphers  = rsnlayer.pairwise_cipher_suites
+
+			for cipher in ciphers:
+				retval += aciphers.get(cipher.cipher) if not retval else ("/"+aciphers.get(cipher.cipher))
+
+		elif pkt.haslayer(Dot11EltMicrosoftWPA):
+			wpalayer = pkt.getlayer(Dot11EltMicrosoftWPA)
+			ciphers  = wpalayer.pairwise_cipher_suites
+
+			for cipher in ciphers:
+				retval += aciphers.get(cipher.cipher) if not retval else ("/"+aciphers.get(cipher.cipher))
 
 		return retval
 
-	def extract_cipher(self, pkt):
-		return
-
 	def extract_auth(self, pkt):
-		return
+		retval = ''
+		aakms = {
+			1: 'MGT',
+			2: 'PSK'
+		}
+
+		if pkt.haslayer(Dot11EltRSN):
+			rsnlayer = pkt.getlayer(Dot11EltRSN)
+			akms     = rsnlayer.akm_suites
+
+			for akm in akms:
+				retval += aakms.get(akm.suite) if not retval else ("/"+akms.get(akm.suite))
+
+		elif pkt.haslayer(Dot11EltMicrosoftWPA):
+			rsnlayer = pkt.getlayer(Dot11EltMicrosoftWPA)
+			akms     = rsnlayer.akm_suites
+
+			for akm in akms:
+				retval += aakms.get(akm.suite) if not retval else ("/"+akms.get(akm.suite))
+
+		return retval
+
+	def toadd(self, bss):
+		retval = True
+
+		for ap in list(self.__ACCESSPOINTS.keys()):
+			if bss == ap:
+				retval = False
+				break
+
+		return retval
 
 	def filter(self, pkt):
 		if pkt.haslayer(Dot11Beacon):
@@ -109,15 +177,23 @@ class SNIFFER:
 				'power': power,
 				'encryption': encryption,
 				'cipher': cipher,
-				'auth': auth
+				'auth': auth,
 			}
 
-			if toappend not in self.__ACCESSPOINTS:
-				self.__ACCESSPOINTS.append(
-					toappend
-				)
-				print(toappend)
+			self.__ACCESSPOINTS[ bssid ] = toappend
+			print(self.__ACCESSPOINTS)
+
+	def hopper(self):
+		while True:
+			ch = random.choice(self.channels)
+			subprocess.call(['iwconfig', self.interface, 'channel', ch])
+
+			time.sleep(0.5)
 
 	def sniff(self):
+		t = threading.Thread(target=self.hopper)
+		t.daemon = True
+		t.start()
+
 		sniff(iface=self.interface, prn=self.filter)
 		sys.stdout.write("\r")
